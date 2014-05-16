271ba8ceac1fd03f0157e062ba8179f9f116d454
am2100 <jimnoble@xjjz.co.uk>
2014-05-16 07:26:26 -0300

    Add rock_paper_scissors hw files

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -4,11 +4,28 @@ class RockPaperScissors
   class NoSuchStrategyError < StandardError ; end
 
   def self.winner(player1, player2)
-    # YOUR CODE HERE
+    game = "#{player1[1] + player2[1]}"
+    raise NoSuchStrategyError.new('Strategy must be one of R,P,S') unless game =~/\A[RPS]{2}\z/
+    game =~ /(SR|PS|RP)/ ? player2 : player1
   end
 
   def self.tournament_winner(tournament)
-    # YOUR CODE HERE
-  end
+    @@this_round = tournament.flatten.each_slice(2).to_a # ; p @@this_round
+    @@next_round = Array.new # ; p @@next_round
 
+    num_rounds = (Math.log(@@this_round.length/2)/Math.log(2)).to_i + 1
+    num_rounds.times do |round|
+      until @@this_round.empty?
+        p1 = @@this_round.shift
+        p2 = @@this_round.shift
+        @@next_round << self.winner(p1, p2)
+      end
+      @@this_round = @@next_round # ; p @@this_round
+      @@next_round = [] unless @@next_round.length == 1 # ; p @@next_round
+    end
+#    p @@next_round[0]
+#     return "banana"
+     @@next_round[0]
+  end
 end
+
